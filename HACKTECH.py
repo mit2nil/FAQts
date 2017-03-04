91b5a96c45f3401930f32d71d12e1ef0bd9a1176
Ankit Mittal <ankit.mittal@alumni.ubc.ca>
2017-03-04 04:55:52 -0800

    Add files via upload

---
+++ HACKTECH.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+#@app.route("/")
+def my_form():
+    return render_template("my-form.html")
+
+@app.route('/', methods=['GET'])
+def my_form_post():
+
+    text = request.form['search']
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == "__main__":
+    app.run()
+
+
+    
